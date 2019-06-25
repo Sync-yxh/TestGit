44bbecc6dd68ebc03968c1aa02d556fceb0e6d54
Sync-yxh <836343716@qq.com>
2019-06-25 11:01:54 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+vector<int> postorderTraversal(TreeNode* root) {
+    stack<TreeNode*> stk;
+    vector<int> result;
+    if(root==NULL)
+        return result;
+    TreeNode* now = root;
+    TreeNode* last = NULL;
+    while(now != NULL)
+    {
+        stk.push(now);
+    }
+    while(!stk.empty())
+    {
+        now = stk.top();
+        if(now->right != NULL && now->right != last)
+        {
+            now = now->right;
+            while(now != NULL)
+            {
+                stk.push(now);
+                now = now->left;
+            }
+        }
+        else
+        {
+            result.push_back(now->val);
+            last = now;
+            stk.pop();
+        }
+    }
+    return result;
+}
